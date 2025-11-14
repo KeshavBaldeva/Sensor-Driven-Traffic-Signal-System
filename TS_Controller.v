3452d2d8f64f52684433758db72c9097807f2b91
Keshav Baldeva <152970391+KeshavBaldeva@users.noreply.github.com>
2025-11-14 19:58:53 +0530

    Update TS_Controller.v

--- TS_Controller.v
+++ TS_Controller.v
@@ -1,96 +1,149 @@
 `timescale 1ns / 1ps
 
+module TS_Controller (
+    input  clk,
+    input  sensor,
+    input  clear,
+    output reg R_ctry, G_ctry, Y_ctry,
+    output reg R_hiwy, G_hiwy, Y_hiwy
+);
 
-module TS_Controller(
-input clk,sensor,clear,
-output reg R_ctry,G_ctry,Y_ctry,R_hiwy,G_hiwy,Y_hiwy
-    );
+    parameter s0 = 2'b00;
+    parameter s1 = 2'b01;
+    parameter s2 = 2'b10;
+    parameter s3 = 2'b11;
     
-    reg [1:0]state=2'b00,next_state;
-    parameter s0=2'b00,s1=2'b01,s2=2'b10,s3=2'b11;
-    
-    integer count=0;
+    parameter CLK_FREQ = 50000000;//Assumed that the clock frequency is 50MHz
+
+    reg [1:0] state, next_state;
+    reg [5:0] count;
     reg count_reset;
     
+    reg sensor_sync_d;
+    reg sensor_sync;
+    
+    reg [25:0] tick_counter; //26 bit counter to calculate 1sec
+    reg sec_tick;
+
+// synchronizer for sensor
+    always@(posedge clk) 
+    begin
+        if (clear) begin
+            sensor_sync_d <= 1'b0;
+            sensor_sync <= 1'b0;
+        end else
+         begin
+            sensor_sync_d <= sensor;
+            sensor_sync <= sensor_sync_d;
+         end
+    end
+    
+   
     always@(posedge clk)
     begin
-      if(count_reset==1)
-      begin
-        count=0;
-        count_reset=0;
-      end
-      else
-      begin
-        count = count+1;
-      end
+        if(clear)
+         begin
+            tick_counter <= 0;
+            sec_tick <= 1'b0;
+         end
+        else if(tick_counter == CLK_FREQ - 1)
+         begin
+            tick_counter <= 0;
+            sec_tick<= 1'b1;
+         end
+        else
+         begin
+            tick_counter <= tick_counter + 1;
+            sec_tick <= 1'b0;
+         end
     end
-    
-    always@(posedge clk)
+
+
+    always@(posedge clk) 
     begin
-      if(clear)
-      state<=s0;
-      else
-      state<=next_state;
+        if(clear)
+            state <= s0;
+        else
+            state <= next_state;
     end
-    
-    always@(*)
+
+// seconds counter
+    always@(posedge clk) 
     begin
-      case(state)
-      s0:
-         if(count>=60 & sensor==1)
-          next_state<=s1;
-         else
-          next_state<=s0;
-          
-      s1:
-         if(count>=10)
-          next_state<=s2;
-         else
-          next_state<=s1;
-          
-      s2:
-         if(count<=30 & sensor==1)
-          next_state<=s2;
-         else
-          next_state<=s3;
-          
-      s3:
-         if(count>=10)
-          next_state<=s0;
-         else
-          next_state<=s3;
-      endcase
+        if(clear)
+            count <= 6'b0;
+        else if (count_reset)
+            count <= 6'b0;
+        else if (sec_tick)
+            count <= count + 1;
     end
     
-    always@(*)
-    begin
-      count_reset=1;
-      {R_ctry,G_ctry,Y_ctry,R_hiwy,G_hiwy,Y_hiwy}=6'b000000;
-      case(state)
-        s0:
-           begin
-             R_ctry<=1'b1;
-             G_hiwy<=1'b1;
-           end
-           
-        s1:
-           begin
-             R_ctry<=1'b1;
-             Y_hiwy<=1'b1;
-           end
-           
-        s2:
-           begin
-             G_ctry<=1'b1;
-             R_hiwy<=1'b1;
-           end
-           
-        s3:
-           begin
-             Y_ctry<=1'b1;
-             R_hiwy<=1'b1;
-           end   
-      endcase
+    //logic for the next stae
+    always@(*) begin
+        next_state = state;
+        count_reset = 1'b0; 
+
+        case(state)
+            s0: begin
+                if (count >= 60 && sensor_sync == 1) 
+                begin
+                    next_state = s1;
+                    count_reset = 1'b1;
+                end
+            end
+            
+            s1: begin
+                if (count >= 10) 
+                begin
+                    next_state = s2;
+                    count_reset = 1'b1;
+                end
+            end
+            
+            s2: begin
+                if (sensor_sync == 0 || count > 30) 
+                begin
+                    next_state = s3;
+                    count_reset = 1'b1;
+                end
+            end
+            
+            s3: begin
+                if (count >= 10)
+                begin
+                    next_state = s0;
+                    count_reset = 1'b1;
+                end
+            end
+        endcase
     end
-    
+
+//output logic
+    always@(*) begin
+        {R_ctry, G_ctry, Y_ctry} = 3'b100;
+        {R_hiwy, G_hiwy, Y_hiwy} = 3'b100;
+        
+        case(state)
+            s0: begin
+                G_hiwy = 1'b1;
+                R_hiwy = 1'b0;
+            end
+            
+            s1: begin
+                Y_hiwy = 1'b1;
+                R_hiwy = 1'b0;
+            end
+                
+            s2: begin
+                G_ctry = 1'b1;
+                R_ctry = 1'b0;
+            end
+                
+            s3: begin
+                Y_ctry = 1'b1;
+                R_ctry = 1'b0;
+            end
+        endcase
+    end
+
 endmodule
